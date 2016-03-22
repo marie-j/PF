1d609aa22459b8966f3ef5b25bf3d28afb5114ba
marie <marie.jones@etudiant.univ-lille1.fr>
2016-03-22 11:12:30 +0100

    decommenter partie Gloss

--- TP2/dragon.hs
+++ TP2/dragon.hs
@@ -3,11 +3,11 @@
 
 --TP2 PF : Partie 3 Dragon
 
---import Graphics.Gloss
+import Graphics.Gloss
 
---main = animate (InWindow "Dragon" (500, 500) (0, 0)) white (dragonAnime (50,250) (450,250))
+main = animate (InWindow "Dragon" (500, 500) (0, 0)) white (dragonAnime (50,250) (450,250))
 
---dragonAnime a b t = Line (dragonOrdre a b (round t `mod` 20))
+dragonAnime a b t = Line (dragonOrdre a b (round t `mod` 20))
 
 pointAintercaler::Point->Point->Point
 pointAintercaler x y =
